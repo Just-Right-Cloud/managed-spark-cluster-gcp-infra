7a4c1bde82c0b406c2b9cd7a1384202bc18c890e
Tanchwa <dres2dogs@gmail.com>
2025-08-15 03:06:08 +0800

    added resource for google service api config

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
+resource "google_project_service" "container" {
+  project = var.project_id
+  service = "container.googleapis.com"
+}
+
 resource "google_container_cluster" "main" {
   name     = "spark-cluster"
   location = "asia-south1"
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "project_id" {
+  description = "The ID of the project in which the resources will be created."
+  type        = string
+  default     = "eighth-duality-468907-t2"
+}
