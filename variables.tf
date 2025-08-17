8891a368e93d12fb54816192146f23a4e6bd80c3
CharonWare <62080434+CharonWare@users.noreply.github.com>
2025-08-17 12:49:46 +0100

    Implement network module (#2)
    * implement network module to create VPC required for GKE
    
    * implement network module to create VPC required for GKE

--- variables.tf
+++ variables.tf
@@ -3,3 +3,9 @@ variable "project_id" {
   type        = string
   default     = "eighth-duality-468907-t2"
 }
+
+variable "location" {
+  description = "The region to use for infrastructure."
+  type        = string
+  default     = "asia-south1"
+}
