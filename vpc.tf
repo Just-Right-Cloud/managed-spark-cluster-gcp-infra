b7a71b5f9c56c0a34a244ca814e81d239d22ff89
Charon <admin@cloudtinkerers.com>
2025-08-17 11:11:42 +0100

    implement network module to create VPC required for GKE

---
+++ vpc.tf
@@ -0,0 +1,21 @@
+module "network" {
+  source  = "terraform-google-modules/network/google"
+  version = "11.1.1"
+
+  project_id = var.project_id
+  network_name = "gke-vpc"
+  routing_mode = "REGIONAL"
+
+  subnets = [
+    {
+      subnet_name   = "gke-subnet"
+      subnet_ip     = "10.0.0.0/16"
+      subnet_region = var.location
+
+      secondary_ip_ranges = {
+        pods     = "10.1.0.0/16"
+        services = "10.2.0.0/20"
+      }
+    }
+  ]
+}
