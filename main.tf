c620f5c7e033a8c1a3a91068768289aa6fe672a4
Charon <admin@cloudtinkerers.com>
2025-08-17 12:40:44 +0100

    implement network module to create VPC required for GKE

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ resource "google_project_service" "container" {
 
 resource "google_container_cluster" "main" {
   name     = "spark-cluster"
-  location = "asia-south1"
+  location = var.location
 
   enable_autopilot   = true
   initial_node_count = 2
