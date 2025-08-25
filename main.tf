c25ec4f9a6d1ab5aabc65c7b1c64713ec3cf6b13
Tanchwa <dres2dogs@gmail.com>
2025-08-25 11:05:55 +0800

    added missing file function

--- main.tf
+++ main.tf
@@ -14,22 +14,22 @@ resource "kubernetes_namespace" "argo" {
   }
 }
 
-#data "http" "argo_crds" {
-#  url = "https://github.com/argoproj/argo-cd/tree/master/manifests/crds"
-#}
+data "http" "argo_crds" {
+  url = "https://github.com/argoproj/argo-cd/tree/master/manifests/crds"
+}
 
 data "http" "argo_operator" {
   url = "https://github.com/argoproj/argo-cd/blob/master/manifests/install.yaml"
 }
 
-#resource "kubernetes_manifest" "argo_crds" {
-#  for_each   = toset(provider::kubernetes::manifest_decode_multi(data.http.argo_crds.body))
-#  manifest   = each.value
-#  depends_on = [kubernetes_namespace.argo]
-#}
+resource "kubernetes_manifest" "argo_crds" {
+  for_each   = toset(provider::kubernetes::manifest_decode_multi(file(data.http.argo_crds.response_body)))
+  manifest   = each.value
+  depends_on = [kubernetes_namespace.argo]
+}
 
 resource "kubernetes_manifest" "argo_operator" {
-  for_each   = toset(provider::kubernetes::manifest_decode_multi(data.http.argo_operator.body))
+  for_each   = toset(provider::kubernetes::manifest_decode_multi(file(data.http.argo_operator.response_body)))
   manifest   = each.value
   depends_on = [kubernetes_namespace.argo]
 }
