68ca1718c3722dd33c93a6a71455efa1f792f28e
Tanchwa <dres2dogs@gmail.com>
2025-12-29 21:05:38 -0500

    added cloudflare api token to config

---
+++ argo-oauth.tf
@@ -0,0 +1,5 @@
+#resource "google_iam_ouath_client" "argocd" {
+#  ouath_client_id = "argocd"
+#  display_name    = "Argo CD OAuth Client"
+#  redirect_uris   = ["https://argo-public.andrewsutliff.com/api/dex/callback"]
+#}
