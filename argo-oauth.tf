aca8e62fac080f30c9cf652bb8939219cb5279f7
Tanchwa <dres2dogs@gmail.com>
2025-12-30 16:51:17 -0500

    added oidc for google auth

--- argo-oauth.tf
+++ argo-oauth.tf
@@ -1,5 +1,14 @@
-#resource "google_iam_ouath_client" "argocd" {
-#  ouath_client_id = "argocd"
-#  display_name    = "Argo CD OAuth Client"
-#  redirect_uris   = ["https://argo-public.andrewsutliff.com/api/dex/callback"]
-#}
+resource "google_iam_ouath_client" "argocd" {
+  ouath_client_id       = "argocd"
+  location              = var.location
+  display_name          = "Argo CD OAuth Client"
+  allowed_redirect_uris = ["https://${var.domain_name}.${var.dns_zone_name}/api/dex/callback"]
+  allowed_scopes        = ["openid"]
+}
+
+resource "google_iam_oauth_client_credential" "argocd" {
+  oauthclient                = google_iam_oauth_client.argocd.oauth_client_id
+  location                   = google_iam_oauth_client.argocd.location
+  oauth_client_credential_id = "argocd-credential"
+  display_name               = "Argo CD OAuth Client Credential"
+}
