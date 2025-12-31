00384bcc4e1f46a1688fe473fe37eba25f8a835d
Tanchwa <89235018+Tanchwa@users.noreply.github.com>
2025-12-30 19:35:35 -0500

    removed oauth, reverted back to loadbalancer type' (#28)

--- argo-oauth.tf
+++ argo-oauth.tf
@@ -1,18 +1,18 @@
-resource "google_iam_oauth_client" "argocd" {
-  oauth_client_id       = "argocd"
-  location              = var.location
-  display_name          = "Argo CD OAuth Client"
-  allowed_redirect_uris = ["https://${var.domain_name}.${var.dns_zone_name}/api/dex/callback"]
-  allowed_scopes        = ["openid"]
-  allowed_grant_types = [
-    "AUTHORIZATION_CODE_GRANT",
-    "REFRESH_TOKEN_GRANT",
-  ]
-}
-
-resource "google_iam_oauth_client_credential" "argocd" {
-  oauthclient                = google_iam_oauth_client.argocd.oauth_client_id
-  location                   = google_iam_oauth_client.argocd.location
-  oauth_client_credential_id = "argocd-credential"
-  display_name               = "Argo CD OAuth Client Credential"
-}
+#resource "google_iam_oauth_client" "argocd" {
+#  oauth_client_id       = "argocd"
+#  location              = var.location
+#  display_name          = "Argo CD OAuth Client"
+#  allowed_redirect_uris = ["https://${var.domain_name}.${var.dns_zone_name}/api/dex/callback"]
+#  allowed_scopes        = ["openid"]
+#  allowed_grant_types = [
+#    "AUTHORIZATION_CODE_GRANT",
+#    "REFRESH_TOKEN_GRANT",
+#  ]
+#}
+#
+#resource "google_iam_oauth_client_credential" "argocd" {
+#  oauthclient                = google_iam_oauth_client.argocd.oauth_client_id
+#  location                   = google_iam_oauth_client.argocd.location
+#  oauth_client_credential_id = "argocd-credential"
+#  display_name               = "Argo CD OAuth Client Credential"
+#}
